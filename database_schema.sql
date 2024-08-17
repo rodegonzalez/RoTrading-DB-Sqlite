96d3cfa0dd2e706749d9721f461846d99cd0c901
Rode Gonzalez <rode.gonzalez@gmail.com>
2024-08-17 10:35:56 +0100

    Clean files

--- database_schema.sql
+++ database_schema.sql
@@ -1,9 +1,3 @@
--- 
--- rotrading
--- v1.0.0
--- 20230404
--- 
-
 CREATE SCHEMA `rotrading` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
  
 use rotrading;
--- database_test_items.sql
+++
@@ -1,6 +0,0 @@
-create table items (
-guid text null,
-id integer primary key,
-itemname text null,
-itemvalue integer null
-)
